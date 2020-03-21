8e416b20e1510af4b818bee972fcebcf0ae9c14a
ANIRUDDHA Kulkarni <41265081+Aniruddha25@users.noreply.github.com>
2020-03-21 16:11:31 +0530

    Add files via upload

---
+++ flipping bits.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : flipping.cpp
+// Author      : Aniruddha
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+#define const (pow(2,32)-1)
+using namespace std;
+long int flipping(unsigned int a)
+{
+
+
+    return const-a;
+}
+
+int main() {
+	  int n;
+	    cin >> n;
+	    while(n--)
+	    {
+	           unsigned int a;
+	           cin >> a;
+	           cout << flipping(a) << endl;
+	    }
+	    return 0;
+}
